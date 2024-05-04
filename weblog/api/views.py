8c0f655ec22707981784d8329a6a1fc4c6dae2bc
ARISUGIANTO <arisugianto698@gmail.com>
2024-05-04 09:51:00 +0700

    membuat API pengguna

--- weblog/api/views.py
+++ weblog/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from api.serializers import UserSerializer
+from django.contrib.auth.models import User
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
