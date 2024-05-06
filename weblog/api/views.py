7614571f0e82c48b3a23a45f3845b4049fc39ed1
ARISUGIANTO <arisugianto698@gmail.com>
2024-05-06 14:42:13 +0700

    menambahkan Post,Comment, Category

--- weblog/api/views.py
+++ weblog/api/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
-from api.serializers import UserSerializer
+from rest_framework import viewsets,permissions
+from api.serializers import UserSerializer,PostSerializer,CommentSerializer,CategorySerializer
 from django.contrib.auth.models import User
+from api.models import Post,Comment, Category
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
+class PostViewset(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
--- weblog/weblog/urls.py
+++ weblog/weblog/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from api import views
@@ -5,9 +6,14 @@ from api import views
 # Buat router
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'posts', views.PostViewset)
+router.register(r'comments', views.CommentViewSet)
+router.register(r'categories', views.CategoryViewSet)
 
 # URLpatterns API
 urlpatterns = [
     # Tambahkan router.urls ke urlpatterns
+    path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
